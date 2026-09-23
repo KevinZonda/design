e9c665287398eb0da466c185ca8bfed5f8a84fb1
KevinZonda <realkevin@tutanota.com>
2026-09-23 09:19:19 +0800

    Wrap App component with ThemeProvider for consistent theming

--- kvzd-design-page/src/main.tsx
+++ kvzd-design-page/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from '@kevinzonda/design'
 import '@kevinzonda/design/style.css'
 import './pages/index.css'
 import App from './pages/App.tsx'
@@ -8,7 +9,9 @@ import App from './pages/App.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <App />
+      <ThemeProvider palette={{ brand: '#000000' }}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </StrictMode>,
 )
