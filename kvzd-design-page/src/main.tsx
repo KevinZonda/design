49d84f10e1ff05a2d6f5548718d41a07dadd2ec8
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2026-09-23 09:53:49 +0800

    Add additional theme palette properties for consistent styling

--- kvzd-design-page/src/main.tsx
+++ kvzd-design-page/src/main.tsx
@@ -9,7 +9,10 @@ import App from './pages/App.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <ThemeProvider palette={{ brand: '#000000' }}>
+      <ThemeProvider palette={{
+        brand: '#000000',
+        templateBackground: '#eeeeee',
+        surfaceBackground: '#eeeeee' }}>
         <App />
       </ThemeProvider>
     </BrowserRouter>
